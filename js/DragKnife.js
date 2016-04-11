fed1343dafae70b8505e2514fb8e6350ff1e5bdf
rafffel <rafffel1@gmail.com>
2016-04-11 23:10:11 +0200

    remove useless

--- js/DragKnife.js
+++ js/DragKnife.js
@@ -1,6 +1,3 @@
-onInflateChange = function(evt) {
-    console.log('triggered');
-}
 onToolChange = function(evt) {
     var tool = $('#ToolChoose').val();
     if(tool == 'DragKnife')
@@ -10,7 +7,20 @@ onToolChange = function(evt) {
         fileParentGroup.updateMatrix();
         var grp = fileObject;
         grp.traverse(function (child){
-            console.log(child);
+            if (child.type == "Line") {
+                 // let's inflate the path for this line. it may not be closed
+                 // so we need to check that.
+                 var clipperArr = [];
+                 // Fix world Coordinates
+                 for (i = 0; i < child.geometry.vertices.length; i++) {
+                    var localPt = child.geometry.vertices[i];
+                    var worldPt = scene.localToWorld(localPt.clone());
+                    var xpos = (worldPt.x + child.position.x);
+                    var ypos = (worldPt.y + child.position.y);
+                    clipperArr.push({X: xpos, Y: ypos});
+                }
+                clipperPaths.push(clipperArr);
+            }
         });
     }
     else
