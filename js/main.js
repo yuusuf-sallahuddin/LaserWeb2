891f0b1f41faf631c6d60639bd6a00eea651233e
openhardwarecoza <peter.plaaswerf@gmail.com>
2016-05-02 11:33:29 +0200

    Fullscreen Button

--- js/main.js
+++ js/main.js
@@ -539,3 +539,27 @@ function printLog(text, color) {
     $('#console').append('<p class="pf" style="color: ' + color + ';">' + text);
     $('#console').scrollTop($("#console")[0].scrollHeight - $("#console").height());
 };
+
+
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+    printLog('Going Fullscreen', successcolor);
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+    printLog('Exiting Fullscreen', successcolor);
+  }
+}
